27524e0d1c7960430db2d0a6289033c05407d792
Bharath Seshadri <chasethesky79@gmail.com>
2020-07-04 14:17:20 -0500

    master
    Initial experimental code changes for Departments / Courses

--- public/js/app-6.js
+++ public/js/app-6.js
@@ -1,321 +1,102 @@
-/*
-  eslint-disable react/prefer-stateless-function, react/jsx-boolean-value,
-  no-undef, jsx-a11y/label-has-for, react/jsx-first-prop-new-line
-*/
-class TimersDashboard extends React.Component {
-  state = {
-    timers: [
-      {
-        title: 'Practice squat',
-        project: 'Gym Chores',
-        id: uuid.v4(),
-        elapsed: 5456099,
-        runningSince: Date.now(),
-      },
-      {
-        title: 'Bake squash',
-        project: 'Kitchen Chores',
-        id: uuid.v4(),
-        elapsed: 1273998,
-        runningSince: null,
-      },
-    ],
-  };
+class CourseSelection extends React.Component {
 
-  handleCreateFormSubmit = (timer) => {
-    this.createTimer(timer);
-  };
-
-  handleEditFormSubmit = (attrs) => {
-    this.updateTimer(attrs);
-  };
-
-  handleTrashClick = (timerId) => {
-    this.deleteTimer(timerId);
-  };
-
-  createTimer = (timer) => {
-    const t = helpers.newTimer(timer);
-    this.setState({
-      timers: this.state.timers.concat(t),
-    });
-  };
-
-  updateTimer = (attrs) => {
-    this.setState({
-      timers: this.state.timers.map((timer) => {
-        if (timer.id === attrs.id) {
-          return Object.assign({}, timer, {
-            title: attrs.title,
-            project: attrs.project,
-          });
-        } else {
-          return timer;
-        }
-      }),
-    });
-  };
-
-  deleteTimer = (timerId) => {
-    this.setState({
-      timers: this.state.timers.filter(t => t.id !== timerId),
-    });
-  };
-
-  render() {
-    return (
-      <div className='ui three column centered grid'>
-        <div className='column'>
-          <EditableTimerList
-            timers={this.state.timers}
-            onFormSubmit={this.handleEditFormSubmit}
-            onTrashClick={this.handleTrashClick}
-          />
-          <ToggleableTimerForm
-            onFormSubmit={this.handleCreateFormSubmit}
-          />
-        </div>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      departmentSelected: 1
+    };
   }
-}
 
-class ToggleableTimerForm extends React.Component {
-  state = {
-    isOpen: false,
-  };
-
-  handleFormOpen = () => {
-    this.setState({ isOpen: true });
-  };
-
-  handleFormClose = () => {
-    this.setState({ isOpen: false });
-  };
-
-  handleFormSubmit = (timer) => {
-    this.props.onFormSubmit(timer);
-    this.setState({ isOpen: false });
-  };
-
-  render() {
-    if (this.state.isOpen) {
-      return (
-        <TimerForm
-          onFormSubmit={this.handleFormSubmit}
-          onFormClose={this.handleFormClose}
-        />
-      );
-    } else {
-      return (
-        <div className='ui basic content center aligned segment'>
-          <button
-            className='ui basic button icon'
-            onClick={this.handleFormOpen}
-          >
-            <i className='plus icon' />
-          </button>
-        </div>
-      );
-    }
+  componentDidMount() {
+    this.props.reFilterCourses(this.state.departmentSelected);
   }
-}
 
-class EditableTimerList extends React.Component {
+  handleChange = (event) => {
+    const { target: { value: departmentSelected }} = event;
+    this.setState({
+      departmentSelected
+    });
+    this.props.reFilterCourses(departmentSelected)
+  };
+
   render() {
-    const timers = this.props.timers.map((timer) => (
-      <EditableTimer
-        key={timer.id}
-        id={timer.id}
-        title={timer.title}
-        project={timer.project}
-        elapsed={timer.elapsed}
-        runningSince={timer.runningSince}
-        onFormSubmit={this.props.onFormSubmit}
-        onTrashClick={this.props.onTrashClick}
-      />
+    const departmentsTemplate = this.props.departments.map(v => (
+        <option key={v.id} value={v.id}>{v.name}</option>
+    ));
+    const coursesTemplate = this.props.courses.map(c => (
+        <option key={c.id} value={c.id}>{c.name}</option>
     ));
     return (
-      <div id='timers'>
-        {timers}
-      </div>
-    );
+        <div>
+        <label>Select a department</label>
+        <select value={this.state.departmentSelected} onChange={this.handleChange}>
+          {departmentsTemplate}
+        </select>
+          <label>Courses</label>
+          <select>
+            {coursesTemplate}
+          </select>
+        </div>
+     )
   }
 }
 
-class EditableTimer extends React.Component {
+class Container extends React.Component {
+
   state = {
-    editFormOpen: false,
+    courses: [],
+    departmentsAndCourses :[
+      {
+        department : {
+          id: 1,
+          name: 'Core'
+        },
+        courses: [{
+          id: 1,
+          name: 'learnyouncode'
+        },
+        {
+          id: 2,
+          name: 'stream-adventure'
+       }]
+      },
+      {
+        department: {
+          id: 2,
+          name: 'Elective'
+        },
+        courses: [{
+          id: 3,
+          name: 'Functional JavaScript'
+        },
+        {
+          id: 4,
+          name: 'Shader School'
+        }]
+      }
+    ]
   };
 
-  handleEditClick = () => {
-    this.openForm();
-  };
+  fetchDepartments = () => this.state.departmentsAndCourses.map(({ department}) => department);
 
-  handleFormClose = () => {
-    this.closeForm();
-  };
-
-  handleSubmit = (timer) => {
-    this.props.onFormSubmit(timer);
-    this.closeForm();
-  };
-
-  closeForm = () => {
-    this.setState({ editFormOpen: false });
-  };
-
-  openForm = () => {
-    this.setState({ editFormOpen: true });
-  };
-
-  render() {
-    if (this.state.editFormOpen) {
-      return (
-        <TimerForm
-          id={this.props.id}
-          title={this.props.title}
-          project={this.props.project}
-          onFormSubmit={this.handleSubmit}
-          onFormClose={this.handleFormClose}
-        />
-      );
-    } else {
-      return (
-        <Timer
-          id={this.props.id}
-          title={this.props.title}
-          project={this.props.project}
-          elapsed={this.props.elapsed}
-          runningSince={this.props.runningSince}
-          onEditClick={this.handleEditClick}
-          onTrashClick={this.props.onTrashClick}
-        />
-      );
-    }
-  }
-}
-
-class Timer extends React.Component {
-  componentDidMount() {
-    this.forceUpdateInterval = setInterval(() => this.forceUpdate(), 50);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.forceUpdateInterval);
-  }
-
-  handleTrashClick = () => {
-    this.props.onTrashClick(this.props.id);
+  fetchCoursesForDepartment = (id) => {
+    const courses = this.state.departmentsAndCourses.reduce((acc, element) => {
+      if (element.department.id === Number(id)) {
+        acc = acc.concat(element.courses);
+      }
+      return acc;
+    }, []);
+    this.setState({
+      courses
+    })
   };
 
   render() {
-    const elapsedString = helpers.renderElapsedString(
-      this.props.elapsed, this.props.runningSince
-    );
     return (
-      <div className='ui centered card'>
-        <div className='content'>
-          <div className='header'>
-            {this.props.title}
-          </div>
-          <div className='meta'>
-            {this.props.project}
-          </div>
-          <div className='center aligned description'>
-            <h2>
-              {elapsedString}
-            </h2>
-          </div>
-          <div className='extra content'>
-            <span
-              className='right floated edit icon'
-              onClick={this.props.onEditClick}
-            >
-              <i className='edit icon' />
-            </span>
-            <span
-              className='right floated trash icon'
-              onClick={this.handleTrashClick}
-            >
-              <i className='trash icon' />
-            </span>
-          </div>
+        <div>
+          <CourseSelection departments={this.fetchDepartments()} courses={this.state.courses} reFilterCourses={this.fetchCoursesForDepartment}/>
         </div>
-        <div className='ui bottom attached blue basic button'>
-          Start
-        </div>
-      </div>
-    );
+    )
   }
 }
-
-class TimerForm extends React.Component {
-  state = {
-    title: this.props.title || '',
-    project: this.props.project || '',
-  };
-
-  handleTitleChange = (e) => {
-    this.setState({ title: e.target.value });
-  };
-
-  handleProjectChange = (e) => {
-    this.setState({ project: e.target.value });
-  };
-
-  handleSubmit = () => {
-    this.props.onFormSubmit({
-      id: this.props.id,
-      title: this.state.title,
-      project: this.state.project,
-    });
-  };
-
-  render() {
-    const submitText = this.props.id ? 'Update' : 'Create';
-    return (
-      <div className='ui centered card'>
-        <div className='content'>
-          <div className='ui form'>
-            <div className='field'>
-              <label>Title</label>
-              <input
-                type='text'
-                value={this.state.title}
-                onChange={this.handleTitleChange}
-              />
-            </div>
-            <div className='field'>
-              <label>Project</label>
-              <input
-                type='text'
-                value={this.state.project}
-                onChange={this.handleProjectChange}
-              />
-            </div>
-            <div className='ui two bottom attached buttons'>
-              <button
-                className='ui basic blue button'
-                onClick={this.handleSubmit}
-              >
-                {submitText}
-              </button>
-              <button
-                className='ui basic red button'
-                onClick={this.props.onFormClose}
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  <TimersDashboard />,
-  document.getElementById('content')
-);
+ReactDOM.render(<Container/>, document.getElementById('content'));
