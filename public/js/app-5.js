967120ea67551d3c260a2a15d8eabce655b0f90b
Bharath Seshadri <chasethesky79@gmail.com>
2020-07-03 20:38:15 -0500

    master
    Implemented inline validation functionality in ReactJS

--- public/js/app-5.js
+++ public/js/app-5.js
@@ -1,318 +1,114 @@
-/*
-  eslint-disable react/prefer-stateless-function, react/jsx-boolean-value,
-  no-undef, jsx-a11y/label-has-for, react/jsx-first-prop-new-line
-*/
-class TimersDashboard extends React.Component {
-  state = {
-    timers: [
-      {
-        title: 'Practice squat',
-        project: 'Gym Chores',
-        id: uuid.v4(),
-        elapsed: 5456099,
-        runningSince: Date.now(),
-      },
-      {
-        title: 'Bake squash',
-        project: 'Kitchen Chores',
-        id: uuid.v4(),
-        elapsed: 1273998,
-        runningSince: null,
-      },
-    ],
-  };
+class Field extends React.Component {
 
-  handleCreateFormSubmit = (timer) => {
-    this.createTimer(timer);
-  };
+    state = {
+        value:'',
+        error: ''
+    };
 
-  // Inside TimersDashboard
-  handleEditFormSubmit = (attrs) => {
-    this.updateTimer(attrs);
-  };
+    onChange = (event) => {
+        const { value, name } = event.target;
+        const error = this.props.validate ? this.props.validate(value) : '';
+        this.setState({
+            value,
+            error
+        });
+        this.props.onChange({ name, value, error});
+    };
+ render() {
+     return (
+         <div>
+             <input className={'input-entry'} placeholder={this.props.placeholder} name={this.props.name} value ={this.state.value} error={this.state.error}  onChange={this.onChange}/>
+             <span style= {{color:'red'}}>{this.state.error}</span>
+         </div>
+     )
+   }
+}
 
-  handleTrashClick = (timerId) => {
-    this.deleteTimer(timerId);
-  };
+class Parent extends React.Component {
 
-  createTimer = (timer) => {
-    const t = helpers.newTimer(timer);
-    this.setState({
-      timers: this.state.timers.concat(t),
-    });
-  };
-
-  updateTimer = (attrs) => {
-    this.setState({
-      timers: this.state.timers.map((timer) => {
-        if (timer.id === attrs.id) {
-          return Object.assign({}, timer, {
-            title: attrs.title,
-            project: attrs.project,
-          });
-        } else {
-          return timer;
+    state = {
+        formObject: {
+            name: '',
+            email:''
+        },
+        people : [],
+        errors: {
+            nameError: 'Required',
+            emailError: 'Required'
+        },
+        disabled: true
+    };
+    validateEmail = (email) => {
+        const re = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+        let errors, formObject;
+        if (!re.test(email)) {
+            errors = Object.assign({}, this.state.errors.email, { email: 'Invalid Email'});
+            this.setState({
+                errors,
+                disabled: true
+            });
+            return 'Invalid Email'
         }
-      }),
-    });
-  };
+        const { name } = this.state.formObject;
+        const { nameError } = this.state.errors;
+        formObject = Object.assign({}, this.state.formObject, { name, email } );
+        errors = Object.assign({}, this.state.errors, { emailError: '', nameError});
+        const disabled = this.state.errors.nameError;
+        this.setState({
+            formObject,
+            errors,
+            disabled
+        });
+    };
 
-  // Inside TimersDashboard
-  deleteTimer = (timerId) => {
-    this.setState({
-      timers: this.state.timers.filter(t => t.id !== timerId),
-    });
-  };
+    validateName = (name) => {
+        let errors, formObject;
+        if (!name) {
+            errors = Object.assign({}, this.state.errors.name, { name: 'Required'});
+            this.setState({
+                errors,
+                disabled: true
+            });
+            return 'Name Required';
+        }
+        const { email } = this.state.formObject;
+        const { emailError } = this.state.errors;
+        formObject = Object.assign({}, this.state.formObject, { name, email } );
+        errors = Object.assign({}, this.state.errors, { nameError: '', emailError });
+        const disabled = this.state.errors.emailError;
+        this.setState({
+            formObject,
+            errors,
+            disabled
+        });
+    };
 
-  render() {
-    return (
-      <div className='ui three column centered grid'>
-        <div className='column'>
-          {/* Inside TimersDashboard.render() */}
-          <EditableTimerList
-            timers={this.state.timers}
-            onFormSubmit={this.handleEditFormSubmit}
-            onTrashClick={this.handleTrashClick}
-          />
-          <ToggleableTimerForm
-            onFormSubmit={this.handleCreateFormSubmit}
-          />
-        </div>
-      </div>
-    );
-  }
-}
+    handleChange = ({name ,value, error}) => {
 
-class ToggleableTimerForm extends React.Component {
-  state = {
-    isOpen: false,
-  };
+    };
 
-  handleFormOpen = () => {
-    this.setState({ isOpen: true });
-  };
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const people = [...this.state.people, this.state.formObject];
+        this.setState({
+            people
+        });
+    };
 
-  handleFormClose = () => {
-    this.setState({ isOpen: false });
-  };
-
-  handleFormSubmit = (timer) => {
-    this.props.onFormSubmit(timer);
-    this.setState({ isOpen: false });
-  };
-
-  render() {
-    if (this.state.isOpen) {
-      return (
-        <TimerForm
-          onFormSubmit={this.handleFormSubmit}
-          onFormClose={this.handleFormClose}
-        />
-      );
-    } else {
-      return (
-        <div className='ui basic content center aligned segment'>
-          <button
-            className='ui basic button icon'
-            onClick={this.handleFormOpen}
-          >
-            <i className='plus icon' />
-          </button>
-        </div>
-      );
+    render() {
+        return (
+            <div className={'center-content'}>
+                <form onSubmit={this.handleSubmit}>
+                    <Field name="email" validate={this.validateEmail} onChange={this.handleChange} placeholder="Email"/>
+                    <Field name="name" validate={this.validateName} onChange={this.handleChange} placeholder="Name"/>
+                    <input disabled={this.state.disabled} type='submit'/>
+                </form>
+                <ul>
+                    {this.state.people.map(({name, email}, index) => <li key={index}>{name}, {email}</li>)}
+                </ul>
+            </div>
+        )
     }
-  }
 }
 
-class EditableTimerList extends React.Component {
-  render() {
-    // Inside EditableTimerList.render()
-    const timers = this.props.timers.map((timer) => (
-      <EditableTimer
-        key={timer.id}
-        id={timer.id}
-        title={timer.title}
-        project={timer.project}
-        elapsed={timer.elapsed}
-        runningSince={timer.runningSince}
-        onFormSubmit={this.props.onFormSubmit}
-        onTrashClick={this.props.onTrashClick}
-      />
-    ));
-    return (
-      <div id='timers'>
-        {timers}
-      </div>
-    );
-  }
-}
-
-class EditableTimer extends React.Component {
-  state = {
-    editFormOpen: false,
-  };
-
-  handleEditClick = () => {
-    this.openForm();
-  };
-
-  handleFormClose = () => {
-    this.closeForm();
-  };
-
-  handleSubmit = (timer) => {
-    this.props.onFormSubmit(timer);
-    this.closeForm();
-  };
-
-  closeForm = () => {
-    this.setState({ editFormOpen: false });
-  };
-
-  openForm = () => {
-    this.setState({ editFormOpen: true });
-  };
-
-  render() {
-    if (this.state.editFormOpen) {
-      return (
-        <TimerForm
-          id={this.props.id}
-          title={this.props.title}
-          project={this.props.project}
-          onFormSubmit={this.handleSubmit}
-          onFormClose={this.handleFormClose}
-        />
-      );
-    // Inside EditableTimer
-    } else {
-      return (
-        <Timer
-          id={this.props.id}
-          title={this.props.title}
-          project={this.props.project}
-          elapsed={this.props.elapsed}
-          runningSince={this.props.runningSince}
-          onEditClick={this.handleEditClick}
-          onTrashClick={this.props.onTrashClick}
-        />
-      );
-    }
-  }
-}
-
-class Timer extends React.Component {
-  handleTrashClick = () => {
-    this.props.onTrashClick(this.props.id);
-  };
-
-  render() {
-    // ...
-    const elapsedString = helpers.renderElapsedString(this.props.elapsed);
-    return (
-      <div className='ui centered card'>
-        <div className='content'>
-          <div className='header'>
-            {this.props.title}
-          </div>
-          <div className='meta'>
-            {this.props.project}
-          </div>
-          <div className='center aligned description'>
-            <h2>
-              {elapsedString}
-            </h2>
-          </div>
-          {/* Inside Timer.render() */}
-          <div className='extra content'>
-            <span
-              className='right floated edit icon'
-              onClick={this.props.onEditClick}
-            >
-              <i className='edit icon' />
-            </span>
-            <span
-              className='right floated trash icon'
-              onClick={this.handleTrashClick}
-            >
-              <i className='trash icon' />
-            </span>
-          </div>
-        </div>
-        <div className='ui bottom attached blue basic button'>
-          Start
-        </div>
-      </div>
-    );
-  }
-}
-
-class TimerForm extends React.Component {
-  state = {
-    title: this.props.title || '',
-    project: this.props.project || '',
-  };
-
-  handleTitleChange = (e) => {
-    this.setState({ title: e.target.value });
-  };
-
-  handleProjectChange = (e) => {
-    this.setState({ project: e.target.value });
-  };
-
-  handleSubmit = () => {
-    this.props.onFormSubmit({
-      id: this.props.id,
-      title: this.state.title,
-      project: this.state.project,
-    });
-  };
-
-  render() {
-    const submitText = this.props.id ? 'Update' : 'Create';
-    return (
-      <div className='ui centered card'>
-        <div className='content'>
-          <div className='ui form'>
-            <div className='field'>
-              <label>Title</label>
-              <input
-                type='text'
-                value={this.state.title}
-                onChange={this.handleTitleChange}
-              />
-            </div>
-            <div className='field'>
-              <label>Project</label>
-              <input
-                type='text'
-                value={this.state.project}
-                onChange={this.handleProjectChange}
-              />
-            </div>
-            <div className='ui two bottom attached buttons'>
-              <button
-                className='ui basic blue button'
-                onClick={this.handleSubmit}
-              >
-                {submitText}
-              </button>
-              <button
-                className='ui basic red button'
-                onClick={this.props.onFormClose}
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  <TimersDashboard />,
-  document.getElementById('content')
-);
+ReactDOM.render(<Parent/>, document.getElementById('content'));
